1d1cfa54806479c68bfcb7b0bafc781bfbea451f
Sayan Bhattacharyya <sn1f3rt@outlook.com>
2024-01-07 21:00:01 +0530

    Add support for updating server welcome message
    ... and IPC rewrite tweaks

--- blueprints/fumeguard/routes.py
+++ blueprints/fumeguard/routes.py
@@ -5,7 +5,7 @@ from quartcord import Unauthorized, requires_authorization
 
 from blueprints.fumeguard import fumeguard_bp
 
-from forms import ModLogChannel, MemberLogChannel
+from forms import ModLogChannel, MemberLogChannel, WelcomeMessage
 from factory import discord, fumeguard_client
 from utils import unauthorized
 
@@ -41,35 +41,20 @@ async def _app():
     r = await fumeguard_client.request("get_mutual_guilds", user_id=user.id)
     mutual_guilds = r.response["guilds"]
 
-    guilds = list()
-
-    for guild in await user.get_guilds():
-        if guild.permissions.manage_guild and guild.id in mutual_guilds:
-            guilds.append([guild.id, guild.name])
-
-    return await render_template(
-        "fumeguard/app.html", user=user, guilds=guilds
-    )
+    return await render_template("fumeguard/app.html", user=user, guilds=mutual_guilds)
 
 
 @fumeguard_bp.route("/fumeguard/app/settings/<guild_id>")
 @requires_authorization
 async def _settings(guild_id):
-    try:
-        guild_id = int(guild_id)
-
-    except ValueError:
-        return redirect(url_for("fumeguard._app"))
-
     user = await discord.fetch_user()
 
-    guilds = list()
+    r = await fumeguard_client.request("get_mutual_guilds", user_id=user.id)
+    mutual_guilds = r.response["guilds"]
 
-    for guild in await user.get_guilds():
-        if guild.permissions.manage_guild:
-            guilds.append(guild.id)
+    if guild_id in mutual_guilds.keys():
+        guild_id = int(guild_id)
 
-    if guild_id in guilds:
         r = await fumeguard_client.request("get_channel_list", guild_id=guild_id)
 
         if "error" in r.response:
@@ -77,54 +62,60 @@ async def _settings(guild_id):
 
         channels = list()
 
-        for _channel in r.response["channels"]:
-            channels.append((_channel["id"], f"#{_channel['name']}"))
+        for _id, _name in r.response["channels"].items():
+            channels.append((_id, f"# {_name}"))
 
-        channels.insert(0, (0, "LOGGING DISABLED"))
+        channels.insert(0, (0, "DISABLE LOGGING"))
 
+        # Moderation Logging Form
         mod_log_form = await ModLogChannel().create_form()
-        member_log_form = await MemberLogChannel().create_form()
-
         mod_log_form.channel.choices = channels
-        member_log_form.channel.choices = channels
 
         r = await fumeguard_client.request("get_mod_log_channel", guild_id=guild_id)
         current_mod_log_channel = r.response
-        r = await fumeguard_client.request("get_member_log_channel", guild_id=guild_id)
-        current_member_log_channel = r.response
 
-        if "error" in current_mod_log_channel:
+        if current_mod_log_channel["id"] == 0:
             current_mod_log_channel = "MODERATION LOG DISABLED"
 
         else:
-            for channel in channels:
-                if channel[0] == current_mod_log_channel["id"]:
-                    mod_log_form.channel.default = channel[0]
-                    mod_log_form.process()
-                    break
+            mod_log_form.channel.default = current_mod_log_channel["id"]
+            mod_log_form.process()
+            current_mod_log_channel = f"# {current_mod_log_channel['name']}"
 
-            current_mod_log_channel = f"#{current_mod_log_channel['name']}"
+        r = await fumeguard_client.request("get_member_log_channel", guild_id=guild_id)
+        current_member_log_channel = r.response
 
-        if "error" in current_member_log_channel:
+        mod_log_form.choices.data = json.dumps(channels)
+
+        # Member Logging Form
+        member_log_form = await MemberLogChannel().create_form()
+        member_log_form.channel.choices = channels
+
+        if current_member_log_channel["id"] == 0:
             current_member_log_channel = "MEMBER LOG DISABLED"
 
         else:
-            for channel in channels:
-                if channel[0] == current_member_log_channel["id"]:
-                    member_log_form.channel.default = channel[0]
-                    member_log_form.process()
-                    break
+            member_log_form.channel.default = current_member_log_channel["id"]
+            member_log_form.process()
+            current_member_log_channel = f"# {current_member_log_channel['name']}"
 
-            current_member_log_channel = f"#{current_member_log_channel['name']}"
-
-        mod_log_form.choices.data = json.dumps(channels)
         member_log_form.choices.data = json.dumps(channels)
+
+        # Welcome Message Form
+        welcome_message_form = await WelcomeMessage().create_form()
+
+        r = await fumeguard_client.request("get_welcome_message", guild_id=guild_id)
+        current_welcome_message = r.response
+
+        welcome_message_form.message.default = current_welcome_message["message"] or ""
+        welcome_message_form.process()
 
         return await render_template(
             "fumeguard/settings.html",
             guild_id=guild_id,
             mod_log_form=mod_log_form,
             member_log_form=member_log_form,
+            welcome_message_form=welcome_message_form,
             current_mod_log_channel=current_mod_log_channel,
             current_member_log_channel=current_member_log_channel,
         )
@@ -138,21 +129,14 @@ async def _settings(guild_id):
 )
 @requires_authorization
 async def _update(guild_id, route):
-    try:
-        guild_id = int(guild_id)
-
-    except ValueError:
-        return redirect(url_for("fumeguard._app"))
-
     user = await discord.fetch_user()
 
-    guilds = list()
+    r = await fumeguard_client.request("get_mutual_guilds", user_id=user.id)
+    mutual_guilds = r.response["guilds"]
 
-    for guild in await user.get_guilds():
-        if guild.permissions.manage_guild:
-            guilds.append(guild.id)
+    if guild_id in mutual_guilds.keys():
+        guild_id = int(guild_id)
 
-    if guild_id in guilds:
         if route == "mod_log_channel":
             mod_log_form = await ModLogChannel().create_form()
 
@@ -161,6 +145,18 @@ async def _update(guild_id, route):
 
             if await mod_log_form.validate_on_submit():
                 channel_id = int(mod_log_form.channel.data)
+
+                r = await fumeguard_client.request(
+                    "get_mod_log_channel", guild_id=guild_id
+                )
+                current_mod_log_channel = r.response["id"]
+
+                if channel_id == current_mod_log_channel:
+                    await flash(
+                        "The channel you selected is already the current moderation log channel.",
+                        "warning",
+                    )
+                    return redirect(url_for("fumeguard._settings", guild_id=guild_id))
 
                 r = await fumeguard_client.request(
                     "update_mod_log_channel", guild_id=guild_id, channel_id=channel_id
@@ -186,6 +182,18 @@ async def _update(guild_id, route):
                 channel_id = int(member_log_form.channel.data)
 
                 r = await fumeguard_client.request(
+                    "get_member_log_channel", guild_id=guild_id
+                )
+                current_member_log_channel = r.response["id"]
+
+                if channel_id == current_member_log_channel:
+                    await flash(
+                        "The channel you selected is already the current member log channel.",
+                        "warning",
+                    )
+                    return redirect(url_for("fumeguard._settings", guild_id=guild_id))
+
+                r = await fumeguard_client.request(
                     "update_member_log_channel",
                     guild_id=guild_id,
                     channel_id=channel_id,
@@ -193,6 +201,40 @@ async def _update(guild_id, route):
 
                 if r.response["status"] == 200:
                     await flash("Member log channel has been updated.", "success")
+                    return redirect(url_for("fumeguard._settings", guild_id=guild_id))
+
+                else:
+                    await flash(
+                        "An error occurred while processing your request.", "error"
+                    )
+                    return redirect(url_for("fumeguard._settings", guild_id=guild_id))
+
+        elif route == "welcome_message":
+            welcome_message_form = await WelcomeMessage().create_form()
+
+            if await welcome_message_form.validate_on_submit():
+                message = welcome_message_form.message.data or None
+
+                r = await fumeguard_client.request(
+                    "get_welcome_message", guild_id=guild_id
+                )
+                current_welcome_message = r.response["message"]
+
+                if message == current_welcome_message:
+                    await flash(
+                        "The message you entered is already the current welcome message.",
+                        "warning",
+                    )
+                    return redirect(url_for("fumeguard._settings", guild_id=guild_id))
+
+                r = await fumeguard_client.request(
+                    "update_welcome_message",
+                    guild_id=guild_id,
+                    message=message,
+                )
+
+                if r.response["status"] == 200:
+                    await flash("Welcome message has been updated.", "success")
                     return redirect(url_for("fumeguard._settings", guild_id=guild_id))
 
                 else:
--- forms.py
+++ forms.py
@@ -1,16 +1,27 @@
 from quart_wtf import QuartForm
-from wtforms import SelectField, HiddenField
+from wtforms import SelectField, HiddenField, TextAreaField
+from wtforms.validators import Length
 
 
 class ModLogChannel(QuartForm):
     channel = SelectField(
-        "Select Channel", coerce=int
-    )  # , validators=[DataRequired()])
+        "Select Channel: ",
+        coerce=int,
+    )
     choices = HiddenField()
 
 
 class MemberLogChannel(QuartForm):
     channel = SelectField(
-        "Select Channel", coerce=int
-    )  # , validators=[DataRequired()])
+        "Select Channel: ",
+        coerce=int,
+    )
     choices = HiddenField()
+
+
+class WelcomeMessage(QuartForm):
+    message = TextAreaField(
+        "Enter the welcome message, which will be DMed to a new member "
+        "on joining the server (Leave blank to disable): ",
+        validators=[Length(max=1500)],
+    )
