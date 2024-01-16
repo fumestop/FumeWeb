859cd80e034ac4ed626a2df303399e462796ba42
Sayan Bhattacharyya <sn1f3rt@outlook.com>
2024-01-16 22:06:02 +0530

    Small typo fix

--- config.example.py
+++ config.example.py
@@ -5,15 +5,15 @@ SECRET_KEY = "secret_key"
 FUMEGUARD_STANDARD_PORT = 10001
 FUMEGUARD_MULTICAST_PORT = 20001
 FUMEGUARD_INVITE_URL = "fumeguard_invite_url"
-FUMEGUARD_VOTE_URL = "fumeguard_invite_url"
-FUMEGUARD_REVIEW_URL = "fumeguard_invite_url"
+FUMEGUARD_VOTE_URL = "fumeguard_vote_url"
+FUMEGUARD_REVIEW_URL = "fumeguard_review_url"
 
 # FumeTune
 FUMETUNE_STANDARD_PORT = 10002
 FUMETUNE_MULTICAST_PORT = 20002
 FUMETUNE_INVITE_URL = "fumetune_invite_url"
-FUMETUNE_VOTE_URL = "fumetune_invite_url"
-FUMETUNE_REVIEW_URL = "fumetune_invite_url"
+FUMETUNE_VOTE_URL = "fumetune_vote_url"
+FUMETUNE_REVIEW_URL = "fumetune_review_url"
 
 # Community
 COMMUNITY_SERVER_ID = 1234567890
